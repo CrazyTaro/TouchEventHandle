764f4101e2b5e8fbfa372e7ec1dcba82bcfe7fdf
henry <henry@bestapp.us>
2016-04-08 16:51:00 +0800

    新增统一的入口

--- app/src/main/java/com/henrytaro/ct/activity/TouchEventActivity.java
+++ app/src/main/java/com/henrytaro/ct/activity/TouchEventActivity.java
@@ -1,6 +1,7 @@
 package com.henrytaro.ct.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,11 @@ public class TouchEventActivity extends Activity {
         mTestView = (TestView) findViewById(R.id.view_test);
         mBtnSubmit = (Button) findViewById(R.id.btn_submit);
 
+        Intent intent = getIntent();
+        if (intent.getExtras() != null) {
+            int whichDraw = intent.getExtras().getInt("whichDraw");
+            mTestView.setWhickDraw(whichDraw);
+        }
         mBtnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
